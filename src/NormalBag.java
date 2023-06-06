7fd1401a97bed05f4cc559d6fdafa88af750a9a6
ansehdwls <moondj928@gmail.com>
2023-06-06 16:54:43 +0900

    BaekJoon NormalBag G5 DP re:Try

--- src/NormalBag.java
+++ src/NormalBag.java
@@ -1,36 +1,55 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
+class bag {
+    int weight;
+    int value;
+
+    public bag(int weight, int value){
+        this.weight = weight;
+        this.value = value;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+    public int getWeight(){
+        return this.weight;
+    }
+}
 
 public class NormalBag {
-    
+    static bag Goods[];
+    static int DP[][];
     public static void main(String ars[]){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int K = sc.nextInt();
-        int bag[] = new int [N];
-        int weight[] = new int [N];
-        for(int i = 0 ; i < N ; i++) {
-            bag[i] = sc.nextInt();
-            weight[i] = sc.nextInt();
+
+        Goods = new bag[N+1];
+        for(int i = 1 ; i < N+1 ; i++) {
+            Goods[i] = new bag(sc.nextInt() ,sc.nextInt());
         }
 
-        for(int i = 0 ; i < N ; i++) {
-            int a = 0;
-            if(bag[i] < bag[i-1]){
-                a = bag[i-1];
-                bag[i-1] = bag[i];
-                bag[i] = a;
+        DP = new int [N+1][K+1];
 
-                a = weight[i-1];
-                weight[i-1] = weight[i];
-                weight[i] = a;
+        System.out.println(max(N,K));
+    }
 
-                i = 0;
+    public static int max(int N, int K){
+        for(int i = 1 ; i < N+1 ; i++){
+            for(int j = 1 ; j < K+1 ; j++){
+                if(j >= Goods[i].getWeight()){
+                    DP[i][j] = Math.max(DP[i - 1][j], DP[i - 1][j - Goods[i].getWeight()] + Goods[i].getValue());
+                }
+                else{
+                    DP[i][j] = DP[i-1][j];
+                }
             }
         }
-        
-        for(int i = 0 ; i < N ; i++){
 
-        }
+        return DP[N][K];
     }
+    
 }
+
